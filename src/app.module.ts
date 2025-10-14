001afa397a04d150e19f3abbc4cd0643a81d91d4
Ilya Rucavitcyn <ilya.rucavitcyn@gmail.com>
2025-10-14 16:07:33 +0200

    Remove redundant app controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { GitAnalyzerModule } from './git-analyze/routes/git-analyzer.module';
 import { ConfigModule } from './config/config.module';
 
 @Module({
   imports: [ConfigModule, GitAnalyzerModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
